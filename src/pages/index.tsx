6484aa5ef07e26e77f9d165904cecdf1ac34e26a
Hugo Marcelo <hugo.silva@arquivei.com.br>
2021-05-04 17:17:40 -0300

    feat: solve the challenge

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,82 @@
-export default function Home() {
-  return <h1>Hello World</h1>;
+import { Flex, Heading } from '@chakra-ui/react';
+import { GetStaticProps } from 'next';
+import Prismic from '@prismicio/client';
+import Head from 'next/head';
+
+import Banner from '../components/Banner';
+import Divider from '../components/Divider';
+import Features from '../components/Features';
+import Header from '../components/Header';
+import Slider from '../components/Slider';
+
+import { getPrismicClient } from '../services/prismic';
+
+interface HomeProps {
+  continents: {
+    slug: string;
+    data: {
+      title: string;
+      summary: string;
+      image: string;
+    };
+  }[];
 }
+
+export default function Home({ continents }: HomeProps) {
+  return (
+    <Flex direction="column">
+      <Head>
+        <title>WorldTrip - Home</title>
+        <meta property="og:image" content="/ogimage.png" />
+        <meta property="og:image:secure_url" content="/ogimage.png" />
+        <meta name="twitter:image" content="/ogimage.png" />
+        <meta name="twitter:image:src" content="/ogimage.png" />
+        <meta property="og:title" content="WorldTrip" />
+        <meta name="twitter:title" content="WorldTrip" />
+      </Head>
+
+      <Header />
+      <Banner />
+      <Features />
+      <Divider />
+
+      <Heading
+        textAlign="center"
+        fontWeight="500"
+        mb={['5', '14']}
+        fontSize={['lg', '3xl', '4xl']}
+      >
+        Vamos nessa?
+        <br />
+        Então escolha seu continente
+      </Heading>
+
+      <Slider continents={continents} />
+    </Flex>
+  );
+}
+
+export const getStaticProps: GetStaticProps = async () => {
+  const prismic = getPrismicClient();
+
+  const continentsResponse = await prismic.query([
+    Prismic.Predicates.at('document.type', 'continent')
+  ]);
+
+  const continents = continentsResponse.results.map((continent) => {
+    return {
+      slug: continent.uid,
+      data: {
+        title: continent.data.title,
+        summary: continent.data.summary,
+        image: continent.data.slider_image.url
+      }
+    };
+  });
+
+  return {
+    props: {
+      continents
+    }
+  };
+};
